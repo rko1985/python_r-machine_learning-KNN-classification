d40559d6cc3641a5963d1980c071aae819d3858e
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-26 10:16:27 -1000

    R template finished

--- knn.R
+++ knn.R
@@ -2,10 +2,7 @@
 
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
-dataset = dataset[3:5]
-
-# Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+dataset = dataset[,3:5]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -15,29 +12,33 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-training_set[-3] = scale(training_set[-3])
-test_set[-3] = scale(test_set[-3])
+#Feature Scaling
+training_set[,1:2] = scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set[,1:2])
 
-# Fitting K-NN to the Training set and Predicting the Test set results
+#Fitting K-NN to the Training set and Predicting the Test set results
 library(class)
-y_pred = knn(train = training_set[, -3],
-             test = test_set[, -3],
-             cl = training_set[, 3],
-             k = 5,
-             prob = TRUE)
+y_pred = knn(train = training_set[,-3],
+             test = test_set[,-3],
+             cl = training_set[,3],
+             k = 5)
 
-# Making the Confusion Matrix
+
+#Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
 # Visualising the Training set results
+#install.packages('ElemStatLearn')
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+y_grid = knn(train = training_set[,-3],
+             test = grid_set[,-3],
+             cl = training_set[,3],
+             k = 5)
 plot(set[, -3],
      main = 'K-NN (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
@@ -53,7 +54,10 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = knn(train = training_set[, -3], test = grid_set, cl = training_set[, 3], k = 5)
+y_grid = knn(train = training_set[,-3],
+             test = grid_set[,-3],
+             cl = training_set[,3],
+             k = 5)
 plot(set[, -3],
      main = 'K-NN (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
